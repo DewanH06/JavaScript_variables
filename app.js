061f47499c31f1c351c789b4201031851942153a
Dewan <dewan.hasan06gmail.com>
2022-11-29 08:47:22 -0500

    JavaScript_

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+const firstName = "Rosie";
+let lastName = "Cotton"
+var age = 50;
+
+lastName = 'Gamee';
+age = 52;
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log('${language} was first released in ${createdYear}.');
+
+let product = "T-Shirt";
+let prive = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let instock = false;
+let selectedSize = "M";
+
+console.log('${product} in size ${selectedSize} is ${price} with ${stock} in stock.');
+
+let title = "Name of teh wind";
+let author = "Patrick Rothfuss";
+let pageCount = "722";
+let bookMark = "456";
+let hasRead = "true";
+
+console.log(`${title} by ${author} is ${pageCount} page long. Your last bookmark was page ${bookMark}. `);
+
